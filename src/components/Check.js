28c64d2c30663c3ceb623d7b41a4d7c3eebfdf17
Dane <dkoval3@gatech.edu>
2024-01-19 19:33:38 -0700

    Added person, tax, tip fields; added additional util functions

--- src/components/Check.js
+++ src/components/Check.js
@@ -8,8 +8,28 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Button, InputGroup } from 'react-bootstrap';
 import { MinusCircle, PencilSquare } from '@/static/images';
+import { itemizeCheckByPerson } from '@/utils/check-util';
 
-export function AddItem({ setItem, setCheck, setPrice, item, check, price }) {
+function TaxTip({ setTax, setTip }) {
+    return (
+        <Form>
+            <Col>
+                <InputGroup>
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <Form.Control placeholder="Tax" type="number" onChange={event => setTax(event.target.value)} />
+                </InputGroup>
+            </Col>
+            <Col>
+                <InputGroup>
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <Form.Control placeholder="Tip" type="number" onChange={event => setTip(event.target.value)} />
+                </InputGroup>
+            </Col>
+        </Form>
+    );
+}
+
+export function AddItem({ setItem, setCheck, setPrice, setPerson, item, check, price, person }) {
     return(
         <Form>
             <Row>
@@ -23,54 +43,68 @@ export function AddItem({ setItem, setCheck, setPrice, item, check, price }) {
                     </InputGroup>
                 </Col>
                 <Col>
-                    <Button value="Add" onClick={() => setCheck([...check, {item: item, price: price}])} >Add</Button>
+                    <Form.Control value={person} placeholder="Person" onChange={event => setPerson(event.target.value)}/>
                 </Col>
                 <Col>
-                    <Button value="Done" onClick={() => console.log("Done")} >Done</Button>
+                    <Button value="Add" onClick={() => setCheck(check => [...check, {item, price, person}])} >Add</Button>
+                </Col>
+                <Col>
+                    <Button value="Done" onClick={() => console.log(itemizeCheckByPerson(check))} >Done</Button>
                 </Col>
             </Row>
         </Form>
     );
 }
 
+export function AddEditCell(setCheck, check, setItem, item, setPrice, price, setPerson, person, i) {
+    return <td>
+        <Button onClick={() => setCheck(check => check.filter((_, idx) => idx != i))}>
+            <MinusCircle />
+        </Button>
+        <Button onClick={() => {
+            setItem(item);
+            setPrice(price);
+            setPerson(person);
+            setCheck(check.filter((_, idx) => idx != i));
+        } }>
+            <PencilSquare />
+        </Button>
+    </td>;
+}
+
 export default function Check() {
     const [item, setItem] = useState("");
     const [price, setPrice] = useState("");
+    const [person, setPerson] = useState("");
+    const [tax, setTax] = useState();
+    const [tip, setTip] = useState();
     const [check, setCheck] = useState([]);
     
     return(
         <div>
+            <TaxTip {...{setTax, setTip}} />
             <Table>
                 <thead>
                     <tr>
                         <th>Item Name</th>
                         <th>Price</th>
+                        <th>Who Ordered?</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        check.map(({ item, price }, i) => (
+                        check.map(({ item, price, person }, i) => (
                             <tr key={i}>
                                 <td>{item}</td>
                                 <td>{decimalToDollar(price)}</td>
-                                <td>
-                                    <Button onClick={() => setCheck(check.filter((_, idx) => idx != i))}>
-                                        <MinusCircle />
-                                    </Button>
-                                    <Button onClick={() => {
-                                        setItem(item);
-                                        setPrice(price);
-                                        setCheck(check.filter((_, idx) => idx != i));
-                                    }}>
-                                        <PencilSquare />
-                                    </Button>
-                                </td>
+                                <td>{person}</td>
+                                {AddEditCell(setCheck, check, setItem, item, setPrice, price, setPerson, person, i)}
                             </tr>
                         ))
                     }
                 </tbody>
             </Table>
-            <AddItem {...{ setItem, setCheck, setPrice, item, check, price }} />
+            <AddItem {...{ setItem, setCheck, setPrice, setPerson, item, check, price, person }} />
         </div>
     );
 }
--- src/utils/check-util.js
+++ src/utils/check-util.js
@@ -1,4 +1,21 @@
-export const getPerPersonCheck = check => {
-    // TODO: Implement
-    return null;
-};
+/**
+ * Itemize a given check by person
+ * @param {Array<Object>} check - list of items purchased
+ * @param {string} check[].item - item name
+ * @param {string} check[].person - person who purchased the item
+ * @param {string} check[].price - price of the item
+ * @returns {Object} An object with each person's name, their list of items, and the total price
+ */
+export function itemizeCheckByPerson(check) {
+    let itemizedCheck = {};
+    for (let i = 0; i < check.length; i++) {
+        const { item, price, person } = check[i];
+        if (itemizedCheck.hasOwnProperty(person)) {
+            itemizedCheck[person].items.push({item, price});
+            itemizedCheck[person].totalPrice += parseFloat(price);
+        } else {
+            itemizedCheck[person] = {items: [{item, price}], totalPrice: parseFloat(price)};
+        }
+    }
+    return itemizedCheck;
+}
